c0eb3515a9fda3a679a2d5afe3c293355e878ad0
moganbo0817 <moganbo.moganbo@gmail.com>
2022-12-18 21:16:32 +0900

    fea

--- backend/testutil/db.go
+++ backend/testutil/db.go
@@ -16,7 +16,7 @@ func OpenDBForTest(t *testing.T) *sqlx.DB {
 	}
 	db, err := sql.Open(
 		"mysql",
-		fmt.Sprintf("todo:todo@tcp(127.0.0.1:%d)/todo?parseTime=true", port),
+		fmt.Sprintf("temp_project:temp_project@tcp(127.0.0.1:%d)/temp_project?parseTime=true", port),
 	)
 	if err != nil {
 		t.Fatal(err)
