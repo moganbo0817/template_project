03bc1161a9e6326090f2a44bf2ef01127b28041a
moganbo0817 <moganbo.moganbo@gmail.com>
2022-12-23 00:08:21 +0900

    fea

--- backend/handler/list_task.go
+++ backend/handler/list_task.go
@@ -13,27 +13,24 @@ type ListTask struct {
 
 func (lt *ListTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	id := chi.URLParam(r, "userId")
-	ids, _ := strconv.ParseInt(id, 10, 64) // あとできれいに書く
-	task, err := lt.Service.ListTask(ctx, ids)
+	id, err := strconv.ParseInt(chi.URLParam(r, "userId"), 10, 64)
 	if err != nil {
 		RespondJSON(ctx, w, &ErrResponse{
 			Message: err.Error(),
 		}, http.StatusInternalServerError)
 		return
 	}
-	//rsp := task{}
+	task, err := lt.Service.ListTask(ctx, id)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
 	rsp := struct {
 		ID     int    `json:"id"`
 		Title  string `json:"title"`
 		Status string `json:"status"`
 	}{ID: int(task.ID), Title: task.Title, Status: string(task.Status)}
-	// for _, t := range tasks {
-	// 	rsp = append(rsp, task{
-	// 		ID:     t.ID,
-	// 		Title:  t.Title,
-	// 		Status: t.Status,
-	// 	})
-	// }
 	RespondJSON(ctx, w, rsp, http.StatusOK)
 }
