8707492b81dd5d30c1f629d099d8175f7347a4d9
moganbo0817 <moganbo.moganbo@gmail.com>
2022-12-29 14:48:51 +0900

    fea

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext,useState } from 'react';
 import { Button, DatePicker, Space, version } from "antd";
 import "antd/dist/reset.css";
 import Admin from "./view/Admin";
@@ -7,21 +7,25 @@ import Book from "./view/Book";
 import Login from "./view/Login";
 import BookDetail from "./view/BookDetail";
 import TaskDetail from "./view/TaskDetail";
+import {GlobalStoreProvider} from "./components/GlobalStore";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={`/`} element={<Login />} />
-        <Route path={`/book`} element={<Book />} />
-        <Route path={`/task`} element={<Task />} />
-        <Route path={`/bookdetail/:id`} element={<BookDetail />} />
-        <Route path={`/taskdetail/:id`} element={<TaskDetail />} />
-        <Route path={`/taskdetail`} element={<TaskDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalStoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/`} element={<Login />} />
+          <Route path={`/book`} element={<Book />} />
+          <Route path={`/task`} element={<Task />} />
+          <Route path={`/bookdetail/:id`} element={<BookDetail />} />
+          <Route path={`/taskdetail/:id`} element={<TaskDetail />} />
+          <Route path={`/taskdetail`} element={<TaskDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalStoreProvider>
   );
 }
 
